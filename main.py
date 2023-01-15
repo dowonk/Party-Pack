050d5640b17e4de203d79d96ef4eeacbd1a74fa0
dowonk <31142896+dowonk@users.noreply.github.com>
2023-01-14 19:02:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,56 +10,30 @@ client.players = []
 
 @client.event
 async def on_ready():
-    print("Ready")
-
-class Players:
-	def __init__(self, name):
-		self.name = name
-
-class Menu(discord.ui.View):
-	@discord.ui.button(label = "Join Lobby", custom_id = "Join", style = discord.ButtonStyle.secondary)
-	async def button1(self, interaction, button):
-		for p in client.players:
-			if p.name == interaction.user.display_name:
-				await interaction.response.defer()
-				return
-				
-		client.players.append(Players(interaction.user.display_name))
-		
-		embed = discord.Embed(title = 'Dowon\'s Party Pack', description = 'Welcome to Dowon\'s Party Pack! A bot containing a variety of board games that can be played right here on Discord. Created by <@314300380051668994>', colour = discord.Colour.green())
-		embed.add_field(name = 'Players', value = (', '.join(p.name for p in client.players)) if client.players else ('-----'), inline = False)
-		client.menu = await client.menu.edit(embed=embed)
-		await interaction.response.defer()
-
-	@discord.ui.button(label = "Leave Lobby", custom_id = "Leave", style = discord.ButtonStyle.secondary)
-	async def button2(self, interaction, button):
-		for p in client.players:
-			if p.name == interaction.user.display_name:
-				client.players.remove(p)
-				
-		embed = discord.Embed(title = 'Dowon\'s Party Pack', description = 'Welcome to Dowon\'s Party Pack! A bot containing a variety of board games that can be played right here on Discord. Created by <@314300380051668994>', colour = discord.Colour.green())
-		embed.add_field(name = 'Players', value = (', '.join(p.name for p in client.players)) if client.players else ('-----'), inline = False)
-		client.menu = await client.menu.edit(embed=embed)
-		await interaction.response.defer()
-		
-	@discord.ui.select(
-			placeholder = "Select a game",
-			min_values = 1,
-			max_values = 1,
-			options = [
-					discord.SelectOption(label="Just One"),
-					discord.SelectOption(label="Shiritori"),
-					discord.SelectOption(label="The Chameleon")
-			]
-	)
-	async def select_callback(self, select, interaction):
-		if interaction.values[0] == 'The Chameleon':
-			await client.load_extension('chameleon')
+  print("Ready")
 
 @client.command()
 async def play(ctx):
-	embed = discord.Embed(title = 'Dowon\'s Party Pack', description = 'Welcome to Dowon\'s Party Pack! A bot containing a variety of board games that can be played right here on Discord. Created by <@314300380051668994>', colour = discord.Colour.green())
-	embed.add_field(name = 'Players', value = (', '.join(p.name for p in client.players)) if client.players else ('-----'), inline = False)
-	client.menu = await ctx.send(embed=embed, view = Menu())
+	client.channel_id = ctx.channel.id
+	client.menu = None
+	client.players = []
+	
+	try:
+		await client.unload_extension('menu')
+	except:
+		pass
+	try:
+		await client.unload_extension('justone')
+	except:
+		pass
+	try:
+		await client.unload_extension('shiritori')
+	except:
+		pass
+	try:
+		await client.unload_extension('chameleon')
+	except:
+		pass
+	await client.load_extension('menu')
 
 client.run('OTg2ODk3NTIwOTEzNDQwNzc4.GbfKcn.1xpI7M1S922hQ2_bATOhHYdMsmZDdNi25LA9TM')
